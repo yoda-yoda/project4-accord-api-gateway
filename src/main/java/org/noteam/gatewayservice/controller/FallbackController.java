f0fd1b5306ab86c80f3c1967f23dc71924fb69fe
yyubin <hazing120@gmail.com>
2025-02-07 09:45:41 +0900

    fix: spring boot starter web 대신 Reactive 사용하도록 수정

--- src/main/java/org/noteam/gatewayservice/controller/FallbackController.java
+++ src/main/java/org/noteam/gatewayservice/controller/FallbackController.java
@@ -3,12 +3,15 @@ package org.noteam.gatewayservice.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletResponse;
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/fallback")
@@ -18,28 +21,37 @@ public class FallbackController {
     private String fallbackUrl;
 
     @GetMapping("/oauth2")
-    public void oauth2Fallback(HttpServletResponse response) throws IOException {
-        response.sendRedirect(fallbackUrl + "?service=oauth2");
+    public Mono<Void> oauth2Fallback(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create(fallbackUrl + "?service=oauth2"));
+        return response.setComplete();
     }
 
     @GetMapping("/spring")
-    public void springFallback(HttpServletResponse response) throws IOException {
-        response.sendRedirect(fallbackUrl + "?service=spring");
+    public Mono<Void> springFallback(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create(fallbackUrl + "?service=spring"));
+        return response.setComplete();
     }
 
     @GetMapping("/go")
-    public void goFallback(HttpServletResponse response) throws IOException {
-        response.sendRedirect(fallbackUrl + "?service=go");
+    public Mono<Void> goFallback(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create(fallbackUrl + "?service=go"));
+        return response.setComplete();
     }
 
     @GetMapping("/signaling")
-    public void signalingFallback(HttpServletResponse response) throws IOException {
-        response.sendRedirect(fallbackUrl + "?service=signaling");
+    public Mono<Void> signalingFallback(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create(fallbackUrl + "?service=signaling"));
+        return response.setComplete();
     }
 
     @GetMapping("/prometheus")
-    public void prometheusFallback(HttpServletResponse response) throws IOException {
-        response.sendRedirect(fallbackUrl + "?service=prometheus");
+    public Mono<Void> prometheusFallback(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create(fallbackUrl + "?service=prometheus"));
+        return response.setComplete();
     }
-
 }
